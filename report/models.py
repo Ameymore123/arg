d98f7cef9403ba214916c84b78f9ca05cf008dc4
Ameymore123 <142572157+Ameymore123@users.noreply.github.com>
2024-12-27 13:26:02 +0530

    Update models.py

--- report/models.py
+++ report/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Docx_file(models.Model):
-    sr = models.AutoField(primary_key=True)  # Auto-incrementing primary key
-    file = models.FileField(upload_to='docx_files', null=True, blank=True)
-    session_key = models.CharField(max_length=255, null=True, blank=True)  # Allowing session_key to be null or blank
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='docx_files', unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)  # Store the time the file was generated
 
 
 class Prompt(models.Model):
@@ -13,5 +14,4 @@ class Prompt(models.Model):
 
     def __str__(self):
         return 'Topic: ' + self.p_input
-
 
